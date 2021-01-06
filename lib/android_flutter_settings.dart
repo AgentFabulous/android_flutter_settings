c3b7fbd071faecc457075ad60c3f29ec4756fade
HrX03 <dn.bianco03@gmail.com>
2021-01-06 13:43:49 +0100

    settings: Add new key classes to provide an api

--- lib/android_flutter_settings.dart
+++ lib/android_flutter_settings.dart
@@ -1,12 +1,7 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-
-enum SettingType {
-  SECURE,
-  SYSTEM,
-  GLOBAL,
-}
 
 class AndroidFlutterSettings {
   static const MethodChannel _channel =
@@ -18,74 +13,78 @@ class AndroidFlutterSettings {
   }
 
   /// Get methods
-  static Future<String> getString(String setting, SettingType type) async =>
-      await _channel.invokeMethod('getString', {
-        'type': resolveEnum(type),
-        'setting': setting,
-      });
+  static Future<String> getString(SettingKey<String> setting) => _get(setting);
 
-  static Future<int> getInt(String setting, SettingType type) async =>
-      await _channel.invokeMethod('getInt', {
-        'type': resolveEnum(type),
-        'setting': setting,
-      });
+  static Future<int> getInt(SettingKey<int> setting) => _get(setting);
 
-  static Future<bool> getBool(String setting, SettingType type) async =>
-      await _channel.invokeMethod('getBoolean', {
-        'type': resolveEnum(type),
-        'setting': setting,
-      });
+  static Future<bool> getBool(SettingKey<bool> setting) => _get(setting);
 
-  static Future<double> getDouble(String setting, SettingType type) async =>
-      await _channel.invokeMethod('getFloat', {
-        'type': resolveEnum(type),
-        'setting': setting,
-      });
+  static Future<dynamic> _get(SettingKey setting) async {
+    String method;
+
+    switch (setting.valueType) {
+      case SettingValueType.BOOLEAN:
+        method = 'getBoolean';
+        break;
+      case SettingValueType.INT:
+        method = 'getInt';
+        break;
+      case SettingValueType.STRING:
+        method = 'getString';
+        break;
+    }
+
+    return await _channel.invokeMethod(method, {
+      'type': resolveEnum(setting.type),
+      'setting': setting.name,
+    });
+  }
 
   /// Put methods
-  static Future<bool> putString(
-          String setting, String value, SettingType type) async =>
-      await _channel.invokeMethod('putString', {
-        'type': resolveEnum(type),
-        'value': value,
-        'setting': setting,
-      });
+  static Future<bool> putString(SettingKey<String> setting, String value) =>
+      _put(setting, value);
 
-  static Future<bool> putInt(
-          String setting, int value, SettingType type) async =>
-      await _channel.invokeMethod('putInt', {
-        'type': resolveEnum(type),
-        'value': value,
-        'setting': setting,
-      });
+  static Future<bool> putInt(SettingKey<int> setting, int value) =>
+      _put(setting, value);
 
-  static Future<bool> putBool(
-          String setting, bool value, SettingType type) async =>
-      await _channel.invokeMethod('putBoolean', {
-        'type': resolveEnum(type),
-        'value': value,
-        'setting': setting,
-      });
+  static Future<bool> putBool(SettingKey<bool> setting, bool value) =>
+      _put(setting, value);
 
-  static Future<bool> putDouble(
-          String setting, double value, SettingType type) async =>
-      await _channel.invokeMethod('putFloat', {
-        'type': resolveEnum(type),
-        'value': value,
-        'setting': setting,
-      });
+  static Future<bool> _put(SettingKey setting, dynamic value) async {
+    String method;
 
-  static Future<void> setProp(String key, String value) async =>
+    switch (setting.valueType) {
+      case SettingValueType.BOOLEAN:
+        method = 'putBoolean';
+        break;
+      case SettingValueType.INT:
+        method = 'putBoolean';
+        break;
+      case SettingValueType.STRING:
+        method = 'putBoolean';
+        break;
+    }
+
+    return await _channel.invokeMethod(method, {
+      'type': resolveEnum(setting.type),
+      'value': value,
+      'setting': setting.name,
+    });
+  }
+
+  /// Prop methods
+  static Future<void> setProp(PropKey prop, String value) async =>
       await _channel.invokeMethod('setProp', {
-        'key': key,
+        'key': prop.name,
         'value': value,
       });
 
-  static Future<String> getProp(String key) async =>
+  static Future<String> getProp(PropKey prop) async =>
       await _channel.invokeMethod('getProp', {
-        'key': key,
+        'key': prop.name,
       });
 
+  /// Overlay methods
   static Future<bool> reloadAssets(String pkg) async =>
       await _channel.invokeMethod('reloadAssets', {
         'pkg': pkg,
@@ -105,11 +104,59 @@ class AndroidFlutterSettings {
         'pkg': pkg,
         'enable': enable,
       });
-
   static Future<bool> overlaySetEnabledExclusiveInCategory(String pkg) async =>
       await _channel.invokeMethod('overlaySetEnabledExclusiveInCategory', {
         'pkg': pkg,
       });
 
+  /// Utils
   static String resolveEnum(SettingType type) => type.toString().split('.')[1];
+}
+
+@immutable
+class BaseKey {
+  final String name;
+
+  BaseKey._(this.name);
+}
+
+@immutable
+class SettingKey<T> extends BaseKey {
+  final SettingType type;
+  final SettingValueType valueType;
+
+  SettingKey({
+    @required String name,
+    @required this.type,
+  })  : valueType = _getValueTypeFromT(T),
+        super._(name);
+
+  static SettingValueType _getValueTypeFromT(Type t) {
+    if (t is bool) {
+      return SettingValueType.BOOLEAN;
+    }
+    if (t is int || t is double) {
+      return SettingValueType.INT;
+    }
+    return SettingValueType.STRING;
+  }
+}
+
+@immutable
+class PropKey extends BaseKey {
+  PropKey({
+    @required String name,
+  }) : super._(name);
+}
+
+enum SettingType {
+  SECURE,
+  SYSTEM,
+  GLOBAL,
+}
+
+enum SettingValueType {
+  STRING,
+  INT,
+  BOOLEAN,
 }
